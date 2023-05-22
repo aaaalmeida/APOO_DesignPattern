d2dcbc2fbd7e291461583a7bb58d8b2df72dbbfc
aaaalmeida <89162654+aaaalmeida@users.noreply.github.com>
2023-05-21 22:44:45 -0300

    Delete dpStateExercicio directory

--- dpStateExercicio/src/State.java
+++
@@ -1,8 +0,0 @@
-public abstract class State{
-    protected Conta conta;
-    public State(Conta conta) {
-        this.conta = conta;
-    }
-    public abstract void depositar(Double valor);
-    public abstract void sacar(Double valor);
-}
